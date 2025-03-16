0040a03629872c9d6f5abd9eb23c7651c022be17
Amir Kamel <146572749+Amir-Kamel@users.noreply.github.com>
2025-03-16 12:05:36 +0200

    Revert "Amir"

--- TrackStock/orders/admin.py
+++ TrackStock/orders/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Order, Supermarket, OrderItem
-
-admin.site.register(Order)
-admin.site.register(Supermarket)
-admin.site.register(OrderItem)
 
 # Register your models here.
-
--- TrackStock/orders/urls.py
+++ TrackStock/orders/urls.py
@@ -4,13 +4,8 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('', views.order_list, name='order_list'),
     path('create/', views.create_order, name='create_order'),
     path('supermarket/create/', views.create_supermarket, name='create_supermarket'),
+    path('list/', views.order_list, name='order_list'),
     path('supermarket/list/', views.supermarket_list, name='supermarket_list'),
-    path('<int:order_id>/', views.order_details, name='order_details'),
-    path('<int:order_id>/delete/', views.order_delete, name='order_delete'),
-    path('<int:order_id>/delete-product/<int:item_id>/', views.delete_product, name='delete_product'),
-    path('<int:order_id>/add-product/', views.add_product_to_order, name='add_product_to_order'),
-    path("<int:order_id>/edit-product/<int:product_id>/", views.edit_product_in_order, name="edit_product_in_order"),
 ]
