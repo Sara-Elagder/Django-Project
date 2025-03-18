5c07eef1532ea44adb28929d193ce8dfb66c49fd
amir-kamel <amir.kghabrial@gmail.com>
2025-03-19 01:37:11 +0200

    add feature of creating supermarekt with listing them handling them by deleting with order and stock

--- TrackStock/orders/urls.py
+++ TrackStock/orders/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.order_list, name='order_list'),
     path('create/', views.create_order, name='create_order'),
     path('supermarket/create/', views.create_supermarket, name='create_supermarket'),
+    path("supermarket/<int:supermarket_id>/delete/", views.delete_supermarket, name="delete_supermarket"),
     path('supermarket/list/', views.supermarket_list, name='supermarket_list'),
     path('<int:order_id>/', views.order_details, name='order_details'),
     path('<int:order_id>/delete/', views.order_delete, name='order_delete'),
