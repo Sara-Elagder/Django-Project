9ab9b1d566f6567afb9d89ff5448763b231a2b06
hamseldakrory <hamseldakrory14@gmail.com>
2025-03-21 00:07:36 +0200

    feat(error): add custom 404 page

--- TrackStock/accounts/urls.py
+++ TrackStock/accounts/urls.py
@@ -1,11 +1,15 @@
+from django.conf.urls import handler404
 from django.urls import path
+from .views import Custom404View
 from .views import RegisterView, LoginView, LogoutView, ManagerManagementView
 from django.contrib.auth import views as auth_views
 app_name = 'accounts'
 
+handler404 = Custom404View.as_view()
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='registration'),
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('manager-management/', ManagerManagementView.as_view(), name='manager_management'),
+    
 ]
